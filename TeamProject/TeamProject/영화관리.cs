912e245368b078efe3ead55a1d1e54ccf99523b6
Ea-Bell <55992288+Ea-Bell@users.noreply.github.com>
2019-11-25 15:52:54 +0900

    영화관리
    영화관리
    사진없을경우 임의의사진이 올라가도록정해둠

--- TeamProject/TeamProject/영화관리.cs
+++ TeamProject/TeamProject/영화관리.cs
@@ -12,7 +12,6 @@ namespace TeamProject
 {
     public partial class 영화관리 : Form
     {
-
         private String[] 데이터넘기기= new string[5];
         private string image_file = string.Empty;
         private string 경로 = "";
@@ -20,20 +19,10 @@ namespace TeamProject
         public 영화관리()
         {
             InitializeComponent();
-
-
-
-           
-       
-
-      
-
         }
 
         private void 그림찾기btn_Click(object sender, EventArgs e)
-        {
-
-            
+        {            
             //파일 불러오기
             //string image_file = string.Empty;
             // string 경로 = "";
@@ -46,13 +35,13 @@ namespace TeamProject
             else
                 영화포스터.Image = Bitmap.FromFile(image_file);
             경로 = opfl.파일경로명();
-
             경로명.Text = "@\"" + 경로 + "\"";
-
         }
 
         private void 취소_Click(object sender, EventArgs e)
         {
+
+            //버튼 소리낼때 딜레이 걸어서 버튼 종료 시켜야한다.
             Dispose();
         }
 
@@ -60,7 +49,17 @@ namespace TeamProject
         {
             if (image_file == "")
             {
-                MessageBox.Show("이미지를 넣지 않으셨습니다.");
+
+               // MessageBox.Show("이미지를 넣지 않으셨습니다.");
+                DialogResult result1 = MessageBox.Show("정말로 이미지를 넣지 않으시겠습니까?", "이미지 입력확인란", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result1 == System.Windows.Forms.DialogResult.Yes)
+                {
+                    //예를 하면 내가 올린 임의의 사진이 올라가도록 한다.
+                    영화포스터.Image = Bitmap.FromFile(@"C:\Users\user\Documents\GitHub\project\TeamProject\TeamProject\img\오류이미지\파일없음.jpg");
+                    //경로 = @"C:\Users\user\Documents\GitHub\project\TeamProject\TeamProject\img\오류이미지\파일없음.jpg";
+                }
+                else
+                {}
             }
             else if(영화코드txt.Text == ""){
                 MessageBox.Show("영화코드를 넣지 않으셨습니다.");
@@ -74,19 +73,23 @@ namespace TeamProject
                 MessageBox.Show("감독명를 넣지 않으셨습니다.");
             }else if (예고편urlTxt.Text == "")
             {
-                MessageBox.Show("정말로 예고편URL을 넣지 않으시겠습니까?", "URL입력확인란", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                //이미지를 넣었다면 이미지가 항상 같은 경로가 있는지 확인할것. 없으면 없다는 그림을 보여줄것.(내 임의의 그림을 넣어서 확인하자)
-
+                //현재 메시지 박스를 열고서 행하는 행동.
+                DialogResult result1 =MessageBox.Show("정말로 예고편URL을 넣지 않으시겠습니까?", "URL입력확인란", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result1 == System.Windows.Forms.DialogResult.Yes)
+                { }                            
             }
+
             //데이터 넘기기는 영화관리에있는 모든 txt를 영화관리DB클래스로 넘길려고한다.
             데이터넘기기[0] = 영화코드txt.Text;
             데이터넘기기[1] = 영화명txt.Text;
             데이터넘기기[2] = 감독명txt.Text;
-            데이터넘기기[3] = 영화명txt.Text;
+            데이터넘기기[3] = 예고편urlTxt.Text;
             데이터넘기기[4] = 경로;
-
+            //데이터넘기기{영화코드, 영화명, 감독명, 예고편URL, 포스터 사진 경로}
             테스트용_Label.Text = 데이터넘기기[0] +" "+ 데이터넘기기[1] + " " + 데이터넘기기[2] + " " + 데이터넘기기[3] + " " + 데이터넘기기[4];
 
+
+            //영화관리DB에 데이터를 넘길때 데이터들을 set, get시키자. 
 
         }
 
--- TeamProject/TeamProject/영화관리DB.cs
+++ TeamProject/TeamProject/영화관리DB.cs
@@ -8,16 +8,12 @@ using System.Data;
 
 namespace TeamProject
 {
+
+    //디비 접속합시다!
     public class 영화관리DB
     {
         private  static 영화관리DB 디비관리;
         private  string conn = null;
-
-        //4개 만들기. 어떻게? 언제나 디비를 항상 연결해서 쓰기. 잘 모르면 배껴쓰셈. 받아오는건 어떻게? 배열로.
-        public void 검색() { }
-        public void 추가() { }
-        public void 삭제() { }
-        public void 수정() { }
 
         //싱글톤 영화관리 디비 불러올떄 getInstance()로 불러오면된다.
         public static 영화관리DB getInstance()
@@ -28,6 +24,12 @@ namespace TeamProject
             }
             return 디비관리;
         }
+
+        //4개 만들기. 어떻게? 언제나 디비를 항상 연결해서 쓰기. 잘 모르면 배껴쓰셈. 받아오는건 어떻게? 배열로?아 요기서 받지말고 함수(get set)으로 불러오자!
+        public void 검색() { }
+        public void 추가() { }
+        public void 삭제() { }
+        public void 수정() { }
     }
    
 
